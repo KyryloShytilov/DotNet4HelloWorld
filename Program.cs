6375530547ce72f3ec5f9270b1aeb0f9f56fa427
Shitikov Kirill <shitikovkirillt@gmail.com>
2025-09-16 12:48:16 +0200

    Fix print version

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine($".NET CLR Version: {Environment.Version}");
         }
     }
 }
